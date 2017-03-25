14bf0d1f31ca99d1500ae5a37732b09c5b89c974
Jo <winfixit@gmail.com>
2017-03-25 06:57:29 +0100

    Update josm_tested.cmd
    Add parameter for tested or latest

--- JOSM/josm_tested.cmd
+++ JOSM/josm_tested.cmd
@@ -1,3 +1,3 @@
 xcopy /D "preferences.xml" %USERPROFILE%\AppData\Roaming\JOSM\
 
-powershell -noexit -executionpolicy bypass  "& "".\DownloadJOSM.ps1"""
+powershell -noexit -executionpolicy bypass  "& "".\DownloadJOSM.ps1""" -f tested
